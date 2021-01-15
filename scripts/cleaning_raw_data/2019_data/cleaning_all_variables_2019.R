8d7fd47cf8e66d46f4516f7d783bf764b3f51091
Sophie Breitbart <sbreitbart@wesleyan.edu>
2021-01-14 21:17:46 -0500

    Finished cleaning 2019 rmd and getting 2019 data prepped for import into 2020 rmd so that I can compare years in models.

--- scripts/cleaning_raw_data/2019_data/cleaning_all_variables_2019.R
+++ scripts/cleaning_raw_data/2019_data/cleaning_all_variables_2019.R
@@ -10,9 +10,25 @@ library(stringi)
 
 #-------------------
 # Import data
-##-------------------
-# at start of season
-all_raw <- read.csv(
+#-------------------
+# data collection 1
+DC1 <- read.csv(
+  here::here("./CommonGardenExperiment_2019Data/raw_data/DataAnalysis_FirstHeightSurvivalMeasurement_20190610.csv"),
+  header=T,
+  na.strings=c("NO PLANT", "none"),
+  blank.lines.skip=TRUE) %>%
+  as.data.frame()
+
+# data collection 2
+DC2 <- read.csv(
+  here::here("./CommonGardenExperiment_2019Data/raw_data/DataAnalysis_SecondHeightSurvivalMeasurement_20190621.csv"),
+  header=T,
+  na.strings=c("NO PLANT", "none"),
+  blank.lines.skip=TRUE) %>%
+  as.data.frame()
+
+# data collection 3- end of season
+DC3 <- read.csv(
   here::here("./CommonGardenExperiment_2019Data/raw_data/FINAL_3rdmeasuredheight_survival.csv"),
   header=T,
   na.strings=c("NO PLANT", "none"),
@@ -22,34 +38,75 @@ all_raw <- read.csv(
 
 
 ##-------------------
-# Clean data
+# Clean all data
 #-------------------
 
 # Remove rows without plants, empty columns
 # Though 46 holes were dug per 21 rows of the field plot, each hole was not filled with a plant-
 #  ex. Row 1 has empty holes until plot column 13. Removing those empty spreadsheet rows as well
 #  as empty spreadsheet columns.
-c <- is.na(all_raw$Block) == TRUE
-c2 <- c(1:nrow(all_raw))[c]
-all_raw <- all_raw[-c2,]
-names(all_raw)[1]<-"Row"
-all_raw <- all_raw[!is.na(all_raw$Population), ] # there was no plant in this pot anymore... may have been taken by raccoons
+c <- is.na(DC1$Block) == TRUE
+c2 <- c(1:nrow(DC1))[c]
+DC1 <- DC1[-c2,]
+names(DC1)[1]<-"Row"
+DC1 <- DC1[!is.na(DC1$Population), ] # there was no plant in this pot anymore... may have been taken by raccoons
+
+c <- is.na(DC2$Block) == TRUE
+c2 <- c(1:nrow(DC2))[c]
+DC2 <- DC2[-c2,]
+names(DC2)[1]<-"Row"
+DC2 <- DC2[!is.na(DC2$Population), ] # there was no plant in this pot anymore... may have been taken by raccoons
+
+c <- is.na(DC3$Block) == TRUE
+c2 <- c(1:nrow(DC3))[c]
+DC3 <- DC3[-c2,]
+names(DC3)[1]<-"Row"
+DC3 <- DC3[!is.na(DC3$Population), ] # there was no plant in this pot anymore... may have been taken by raccoons
 
 rm(c, c2)
 
+##----------------------------------------
+## Prep DC1 and DC2 before joining with DC3
+#----------------------------------------
+# Calculate total number of ramets per plant for DC2
+
+DC2$Num_Ramets <- rowSums(!is.na(DC2[,c(13:20)]))
+
+# Remove unnecessary columns from DC1 and DC2
+DC1 <- DC1[,-c(12:27)]
+DC2 <- DC2[,-c(13:20, 23:24)]
+
+# rename notes col
+names(DC1)[8]<-"Comment"
+
+# get DC2 measuerer and num ramets for both DCs
+DC1_2 <- merge(x = DC1[,c(1:9)], y = DC2[,-c(9:10,15)],
+               by = c("Row", "Column", "Block", "Population", "Family", "Replicate"),
+               all.x = TRUE,
+               suffix = c('_DC1', '_DC2'))
+# reorder cols
+DC1_2 <- DC1_2[, c(1:9, 12, 15, 10:11, 13, 14)]
+
+
+
+
+
+##----------------------------------------
+## Prep DC3 before joining with DC1 and DC2
+#----------------------------------------
 
 ## Make certain columns factors / check classes are correct
-str(all_raw)
+str(DC3)
 factor_cols <- c("Block", "Population", "Family", "Replicate", "Comment",
                  "Dead_DC1", "Dead_DC2", "Dead_DC3", "Exp_rhiz_binary", "Monarch_dam", "Deer_dam")
-all_raw[factor_cols] <- lapply(all_raw[factor_cols], factor)
+DC3[factor_cols] <- lapply(DC3[factor_cols], factor)
 
 int_cols <- c("Alive_ram", "Dead_ram")
-all_raw[int_cols] <- lapply(all_raw[int_cols], as.integer)
+DC3[int_cols] <- lapply(DC3[int_cols], as.integer)
 
-all_raw[c(16,18,20,22,24,26,28,30,32:38)] <- lapply(
-  all_raw[c(16,18,20,22,24,26,28,30,32:38)], as.numeric)
-str(all_raw)
+DC3[c(16,18,20,22,24,26,28,30,32:38)] <- lapply(
+  DC3[c(16,18,20,22,24,26,28,30,32:38)], as.numeric)
+str(DC3)
 
 
 ## Reclassify Monarch damage & deer damage entries
@@ -60,57 +117,55 @@ str(all_raw)
 # MONARCH DAMAGE
 # Remove all specific indicators
 # can't figure out how to use recode or other more efficient way here
-as.list(unique(all_raw$Monarch_dam)) #entries are 0 1 2 3 5 all no yes
+as.list(unique(DC3$Monarch_dam)) #entries are 0 1 2 3 5 all no yes
 
-all_raw$Monarch_dam = with(all_raw,
+DC3$Monarch_dam = with(DC3,
                            droplevels(replace(Monarch_dam,
                                               is.na(Monarch_dam) | Monarch_dam =="", "0")))
-all_raw$Monarch_dam <- gsub('no', '0', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('0', '0', all_raw$Monarch_dam)
+DC3$Monarch_dam <- gsub('no', '0', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('0', '0', DC3$Monarch_dam)
 
+DC3$Monarch_dam <- gsub('yes', '1', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('all', '1', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('1, 1', '1', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('1, 2', '1', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('2', '1', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('3', '1', DC3$Monarch_dam)
+DC3$Monarch_dam <- gsub('5', '1', DC3$Monarch_dam)
 
-
-all_raw$Monarch_dam <- gsub('yes', '1', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('all', '1', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('1, 1', '1', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('1, 2', '1', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('2', '1', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('3', '1', all_raw$Monarch_dam)
-all_raw$Monarch_dam <- gsub('5', '1', all_raw$Monarch_dam)
-
-as.list(unique(all_raw$Monarch_dam)) #entries are 0 1 2 3 5 all no yes
+as.list(unique(DC3$Monarch_dam)) #entries are 0 1 2 3 5 all no yes
 
 # Make column factor
-all_raw$Monarch_dam <- factor(all_raw$Monarch_dam)
+DC3$Monarch_dam <- factor(DC3$Monarch_dam)
 
 
 # DEER DAMAGE
-all_raw$Deer_dam <- factor(all_raw$Deer_dam)
-as.list(unique(all_raw$Deer_dam)) #entries are 0 1 8 n no y yes
+DC3$Deer_dam <- factor(DC3$Deer_dam)
+as.list(unique(DC3$Deer_dam)) #entries are 0 1 8 n no y yes
 
-all_raw$Deer_dam = with(all_raw,
+DC3$Deer_dam = with(DC3,
                         droplevels(replace(Deer_dam,
                                            is.na(Deer_dam) | Deer_dam =="", "0")))
-all_raw$Deer_dam <- gsub('no', '0', all_raw$Deer_dam)
-all_raw$Deer_dam <- gsub('n', '0', all_raw$Deer_dam)
+DC3$Deer_dam <- gsub('no', '0', DC3$Deer_dam)
+DC3$Deer_dam <- gsub('n', '0', DC3$Deer_dam)
 
 
-all_raw$Deer_dam <- gsub('8', '1', all_raw$Deer_dam)
-all_raw$Deer_dam <- gsub('y', '1', all_raw$Deer_dam)
-all_raw$Deer_dam <- gsub('yes', '1', all_raw$Deer_dam)
-all_raw$Deer_dam <- gsub('1es', '1', all_raw$Deer_dam)
+DC3$Deer_dam <- gsub('8', '1', DC3$Deer_dam)
+DC3$Deer_dam <- gsub('y', '1', DC3$Deer_dam)
+DC3$Deer_dam <- gsub('yes', '1', DC3$Deer_dam)
+DC3$Deer_dam <- gsub('1es', '1', DC3$Deer_dam)
 
-as.list(unique(all_raw$Deer_dam)) #entries are 0 1 8 n no y yes
+as.list(unique(DC3$Deer_dam)) #entries are 0 1 8 n no y yes
 
 # Make column factor
-all_raw$Deer_dam <- factor(all_raw$Deer_dam)
+DC3$Deer_dam <- factor(DC3$Deer_dam)
 
 
 
 
 ## Remove empty columns
-# These columns are empty (all NA's) as seen from str(all_raw)
-all_raw <- all_raw[,-c(32:38)]
+# These columns are empty (all NA's) as seen from str(DC3)
+DC3 <- DC3[,-c(32:38)]
 
 
 
@@ -119,19 +174,19 @@ all_raw <- all_raw[,-c(32:38)]
 #-------------------
 
 ## Ramet 1
-unique(all_raw$Herbiv_ram1) 
+unique(DC3$Herbiv_ram1) 
 ### Replace non-numeric entries with NA and remove dashes (which symbolize no leaf) 
-all_raw$Herbiv_ram1 <- gsub('too small', NA, all_raw$Herbiv_ram1)
-all_raw$Herbiv_ram1 <- gsub('too', NA, all_raw$Herbiv_ram1)
-all_raw$Herbiv_ram1 <- gsub('no leaves', NA, all_raw$Herbiv_ram1)
-all_raw$Herbiv_ram1 <- gsub(' ', ",", all_raw$Herbiv_ram1)
-all_raw$Herbiv_ram1 <- gsub(',-', "", all_raw$Herbiv_ram1)
-all_raw$Herbiv_ram1 <- gsub('-', "", all_raw$Herbiv_ram1)
+DC3$Herbiv_ram1 <- gsub('too small', NA, DC3$Herbiv_ram1)
+DC3$Herbiv_ram1 <- gsub('too', NA, DC3$Herbiv_ram1)
+DC3$Herbiv_ram1 <- gsub('no leaves', NA, DC3$Herbiv_ram1)
+DC3$Herbiv_ram1 <- gsub(' ', ",", DC3$Herbiv_ram1)
+DC3$Herbiv_ram1 <- gsub(',-', "", DC3$Herbiv_ram1)
+DC3$Herbiv_ram1 <- gsub('-', "", DC3$Herbiv_ram1)
 
-unique(all_raw$Herbiv_ram1) 
+unique(DC3$Herbiv_ram1) 
 
 ### Calculate mean herbivory for ramet 1 in new column
-all_raw$Herbivory.mean <- sapply(strsplit(as.character(all_raw$Herbiv_ram1), ",", fixed=T),
+DC3$Herbivory_mean_DC3 <- sapply(strsplit(as.character(DC3$Herbiv_ram1), ",", fixed=T),
                                 function(x) mean(as.numeric(x)))
 
 # IMPORTANT NOTE: In 2019, I measured herbivory on EACH ramet- up to 5 leaves per ramet
@@ -139,8 +194,53 @@ all_raw$Herbivory.mean <- sapply(strsplit(as.character(all_raw$Herbiv_ram1), ","
 # consistency, I'll only calculate herbivory for first ramet in the pot. I'll remove the
 # other ramets' herbivory data from this data frame but will leave them in the raw data file.
 
-all_raw <- all_raw[,-c(19,21,23,25,27,29,31)]
+DC3 <- DC3[,-c(19,21,23,25,27,29,31)]
 
+
+
+
+
+##-----------------------------
+# Calculate total number of ramets per plant
+#-----------------------------
+DC3$Num_Ramets_DC3 <- rowSums(!is.na(DC3[,c(16, 18:24)]))
+
+
+##-----------------------------
+# Calculate total plant height
+#-----------------------------
+DC3$Total_height_DC3 <- NA
+DC3$Total_height_DC3 <- rowSums(DC3[,c(16, 18:24)], na.rm=TRUE)
+
+# remove individual ramet height cols
+DC3 <- DC3[,-c(16, 18:24, 26)]
+
+
+
+
+#----------
+# Join 3 DCs
+#-----------
+# first, add DC3 suffix to appropriate cols (and rename exposed rhizome col for clarity)
+names(DC3)[7]<-"Date_Measured_DC3"
+names(DC3)[8]<-"Comment_DC3"
+names(DC3)[12]<-"Alive_ramets_DC3"
+names(DC3)[13]<-"Dead_ramets_DC3"
+names(DC3)[14]<-"Monarch_damage_DC3"
+names(DC3)[15]<-"Deer_damage_DC3"
+names(DC3)[16]<-"Herbivory_DC3"
+names(DC3)[17]<-"Measurer_DC3"
+names(DC3)[18]<-"Exposed_rhizome_DC1"
+
+# Join DC1_2 and DC3
+DC_all_2019 <- merge(x = DC1_2, y = DC3,
+               by = c("Row", "Column", "Block", "Population", "Family", "Replicate"),
+               all.x = TRUE)
+# reorder cols
+DC_all_2019 <- DC_all_2019 %>% 
+  dplyr::select(sort(names(.)))
+
+DC_all_2019 <- DC_all_2019[, c(27,3, 2, 25, 16, 26, 1, 4:15, 17:24, 28:30)]
 
 
 #-----------------------------------------------------------------------------------
@@ -154,13 +254,29 @@ Distances <- read.csv(here::here(
   "./CommonGardenExperiment_2019Data/clean_data/clean_haversine_Distances.csv"),
   na.strings=c("NO PLANT", "none"), blank.lines.skip=TRUE, header=TRUE, sep=",")
 
+# Join city_dist to DC3
+DC_all_2019 <- merge(x = DC_all_2019, y = Distances, by = "Population", all.x = TRUE)
 
-# Join city_dist to all_raw
-all_raw <- merge(x = all_raw, y = Distances, by = "Population", all.x = TRUE)
+
+### Add urb_score data
+#--------------------------
+# Import urb_index values for each of these rows
+urb_scores <- read.csv(
+  here::here("./CommonGardenExperiment_2020Data/raw_data/Urbanization_Score/Urbanization_Scores_Table.csv"),  header=T, na.strings=c("","NA"))
+
+# get rid of unnecessary cols, rename Patch ID to Pop_ID for joins
+urb_scores <- urb_scores[,c(2,8)]
+names(urb_scores)[1] <- "Pop_ID"
+
+# join
+DC_all_2019 <- merge(DC_all_2019, y = urb_scores, by = "Pop_ID", all.x = TRUE)
+
+# remove "X" column
+DC_all_2019 <- DC_all_2019[,-32]
 
 
 #-------------------
 # Export to new csv
 #-------------------
-write.csv(all_raw,
+write.csv(DC_all_2019,
           here::here("./CommonGardenExperiment_2019Data/clean_data/clean_data_2019KSR.csv"))
