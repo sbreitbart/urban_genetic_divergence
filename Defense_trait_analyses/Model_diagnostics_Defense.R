2a10e237992793efa0079d7b90cb5aa89443b948
Sophie Breitbart <sbreitbart@wesleyan.edu>
2022-02-23 16:56:17 -0500

    Organizing files, made separate file to load libraries and other files for each analysis' model diagnostics

---
+++ Defense_trait_analyses/Model_diagnostics_Defense.R
@@ -0,0 +1,7 @@
+---
+title: "Diagnostics"
+author: "Sophie Breitbart"
+date: "2/23/2022"
+output: html_document
+---
+
---
+++ libraries.R
@@ -0,0 +1,30 @@
+# install.packages("librarian")
+
+librarian::shelf(broom       ,
+                 broom.mixed ,
+                 car         ,
+                 devtools    ,
+                 DHARMa      ,
+                 flextable   ,
+                 forcats     ,
+                 geosphere   ,
+                 ggpubr      ,
+                 glmmTMB     ,
+                 here        ,
+                 Hmisc       ,
+                 lme4        ,
+                 lmerTest    ,
+                 lsmeans     ,
+                 MASS        ,
+                 multcomp    ,
+                 nlme        ,
+                 pbkrtest    ,
+                 scales      ,
+                 stringi     ,
+                 tidyverse   ,
+                 vctrs       ,
+                 vegan
+                 )
+  
+library("tibble", lib.loc="~/R/win-library/3.5") 
+library("tibble", lib.loc="~/R/win-library/3.5") 
