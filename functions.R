95a13d109b256af262faa2d53f29d3118a022513
Sophie Breitbart <sbreitbart@wesleyan.edu>
2022-03-21 16:57:38 -0400

    removed old functions exporting anovas, cleaned up that R script in general

--- functions.R
+++ functions.R
@@ -1,15 +1,15 @@
-# Functions for project
+# Functions
 
+## Create tidy anova tables-----
+### GOALS:
+### 1. Perform car::Anova() on each model in list
+###   -if it's got an interaction, do type III first
+###     -if interaxn p-val < 0.1, retain that result
+###     -else, move on to type II
+### 2. Save anova table w/appropriate SS type as new item in trait's sublist
 
-##### functions to create tidy anova tables #####
-# GOALS:
-# 1. Perform car::Anova() on each model in list
-#   -if it's got an interaction, do type III first
-#     -if interaxn p-val < 0.1, retain that result
-#     -else, move on to type II
-# 2. Save anova table w/appropriate SS type as new item in trait's sublist
-
-# function that IDs if there is >= 1 sig interaction in the type III anova
+### function that IDs if there is >= 1 sig interaction in
+### the type III anova
 ID_intxn <- function(model){
   model.df <- broom.mixed::tidy(car::Anova(model, type = "III"))
   intxn.df <- model.df[grep(":", model.df$term), "p.value"]
@@ -17,7 +17,8 @@ ID_intxn <- function(model){
   return(sig.intxns)
 }
 
-# Next, create best and alt models for individual lists of traits, then do this for the city_dist models, then urb_score models... put underlines in to separate Qs... export!
+### Next, create best and alt models for individual lists
+### of traits
 make_all_anovas_tidy <- function(anova_list){
   return(
     lapply(anova_list, tidy_anova) %>%
@@ -48,10 +49,8 @@ make_all_anovas_tidy <- function(anova_list){
   )
 }
 
-
-
-##### function for doing linear regression on multiple models
-##### (used this for cardenolide functions)
+## Do linear regression on multiple models-----
+# (for cardenolide analysis)
 DoLinearReg <- function(response_var, predictor_var, input_data){
   
   # first, make the formula as a string
@@ -73,8 +72,8 @@ DoLinearReg <- function(response_var, predictor_var, input_data){
 
 
 
-##### function to create tidy ranova table #####
-# for regular g/lmer models
+## Create tidy ranova table-----
+### for regular g/lmer models
 CreateRanovaOutput <- function(lmer_model, var_name){
   
   m1 <- ranova(lmer_model)
@@ -131,7 +130,7 @@ CreateRanovaOutput <- function(lmer_model, var_name){
   return(tab1)
 }
 
-# for boostrapping models
+### for boostrapping models
 CreateRanovaOutput_bootstrap <- function(ranova_fam,
                                          ranova_pop,
                                          var_name){
@@ -170,8 +169,7 @@ CreateRanovaOutput_bootstrap <- function(ranova_fam,
   return(tab1)
 }
 
-
-##### theme for figures #####
+## Create theme for figures-----
 theme_1 <- function(){ 
   
   theme(
@@ -188,185 +186,18 @@ theme_1 <- function(){
   )
 }
 
-
-
-##### Export ANOVAs ##### 
-anova_table_flx <- function(mod1) {
+## Calculate percent change-----
+### Gradient/city_dist models
+Calc_percent_change <- function(ggpredict_object){
+  baseline_numbers <- ggpredict_object %>%
+    dplyr::filter(., x == 0 | x == 70) %>%
+    dplyr::select(c(1,2))
   
-  flx_1 <-  car::Anova(mod1) %>% 
-    tidy() %>%
-    dplyr::mutate_if(.,
-                     is.character,
-                     str_replace_all,
-                     pattern = c("City_dist"),
-                     replacement = c("Distance to City Center")) %>%
-    dplyr::mutate_if(.,
-                     is.character,
-                     str_replace_all,
-                     pattern = c("Urb_score"),
-                     replacement = c("Urbanization Score")) %>%
-    dplyr::mutate_if(.,
-                     is.character,
-                     str_replace_all,
-                     pattern = c("Transect_ID"),
-                     replacement = c("Subtransect")) %>%
-    rename_at(1, ~ "Effect") %>%
-    rename_at(2, ~ "Chi Square" ) %>%
-    rename_at(4, ~ "P" ) %>%
-    # dplyr::select(-3) %>%
-    flextable()  %>%
-    colformat_num(j = c(2,4), digits = 3) %>%
-    flextable::bold(., ~ P  == "< 0.001" | P <= 0.05, ~ P, bold = TRUE) %>%
-    flextable::italic(., ~ P > 0.05 & P <= 0.1, ~ P, italic = TRUE) %>%
-    # set_caption(., caption = "Does Urbanization Affect Year 2 Flowering?") %>%
-    # align(., i = 1, part = "header", align = "center") %>%
-    # add_footer_lines(., paste("Model formula: ", paste0(toString(formula(mod1)[3])))) %>%
-    # italic(., italic = TRUE, part = "footer") %>%
-    align_text_col(., align = "left") %>%
-    align_nottext_col(., align = "center") %>%
-    autofit(., add_w = 0.1, add_h = 0.1) 
+  urb_mean <- baseline_numbers[baseline_numbers$x == min(baseline_numbers$x),]$predicted
   
+  rur_mean <- baseline_numbers[baseline_numbers$x == max(baseline_numbers$x),]$predicted
   
-  # make filename a string (from symbol)
-  # filename1 <- toString(formula(mod1)[[2]])
-  
-  # can't figure out how to remove space from filename so it's staying for now...
-  path_out = here::here("./Figures_Tables/ANOVA_tables_images/ ")
-  
-  save_as_image(flx_1,
-                gsub(" ", "",
-                     paste(path_out,
-                           # filename1,
-                           print(substitute(mod1)),
-                           "_ANOVA.png")))
-  
+  percent_change <- ((urb_mean - rur_mean) / rur_mean) * 100
+  print(baseline_numbers)
+  return(paste0("Percent change, from rural to urban terminus: ", round(percent_change, 1), "%"))
 }
-
-# use _transf function if response var has been transformed
-# use _binomial or _quantitative when I had to fit 2 extra models per var:
-#  binomial and then, with positive values, quantitative
-AIC_compare.city_dist <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         formula(full_model)[[2]],
-                         "_city",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-AIC_compare.city_dist_transf <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]][2]),
-                         "_city",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-AIC_compare.city_dist_transf_binomial <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]][2]),
-                         "_city_binomial",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-AIC_compare.city_dist_transf_quantitative <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]][2]),
-                         "_city_quantitative",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-
-AIC_compare.urb_score <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]]),
-                         "_urbscore",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-AIC_compare.urb_score_transf <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]][2]),
-                         "_urbscore",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-AIC_compare.urb_score_transf_binomial <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]][2]),
-                         "_urbscore_binomial",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-AIC_compare.urb_score_transf_quantitative <- function(full_model, ME_model) {
-  
-  data.frame(Model = c("Full model",
-                       "Main effects only"),
-             AIC = c(AIC(full_model),
-                     AIC(ME_model))) %>%
-    write.csv(gsub(" ", "",
-                   paste(here::here("./Figures_Tables/ANOVA_tables_images/ "),
-                         "AIC_comparison_",
-                         toString(formula(full_model)[[2]][2]),
-                         "_urbscore_quantitative",
-                         # deparse(substitute(full_model)),
-                         ".csv")))
-  
-}
-
-
-# get name of models so I can later export all model names with their formulas-----
-qq <- function(...) sapply(substitute({ ... })[-1], deparse)
